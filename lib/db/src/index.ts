8cf34ac6981d31479ef117164aca192afe807240
yhs <hastar1332@naver.com>
2026-05-31 20:49:11 +0900

    add netlify functions

--- lib/db/src/index.ts
+++ lib/db/src/index.ts
@@ -15,7 +15,7 @@ if (
 
 export const pool = mysql.createPool({
   host: process.env.DB_HOST,
-  port: Number(process.env.DB_PORT || 3306),
+  port: Number(process.env.DB_PORT),
   user: process.env.DB_USER,
   password: process.env.DB_PASSWORD,
   database: process.env.DB_NAME,
